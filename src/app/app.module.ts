f89fee163e308c1a6ae483e0324a43d0aba5a308
Arantish-lucien <arantishsingh@luciensolutions.com>
2024-05-29 01:33:32 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { IntroComponent } from './intro/intro.component';
+import { PackagesComponent } from './packages/packages.component';
+import { TeamComponent } from './team/team.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopNavComponent,
+    IntroComponent,
+    PackagesComponent,
+    TeamComponent,
+    NewsletterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
